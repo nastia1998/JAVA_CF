58cfe7b4f94e0efdfd5dda9a9450942923e08bf1
Anastasiya Leshchanka <a.leshchanka@LeverX.local>
2020-01-13 16:02:36 +0300

    basic structure of project

---
+++ db/.build.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const childproc = require('child_process');
+
+if (fs.existsSync('../package.json')) {
+    // true at build-time, false at CF staging time
+    childproc.execSync('npm install && npm run build', {
+        cwd: '..',
+        stdio: 'inherit'
+    });
+}
